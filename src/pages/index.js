be11253d2ba44287a77bb0a5732be212272df669
Konstantin-exe <konstantinmuellner@gmail.com>
2021-01-18 15:43:17 +0100

    fc

---
+++ src/pages/index.js
@@ -0,0 +1,125 @@
+import React from 'react'
+import Helmet from 'react-helmet'
+
+import Gallery from '../components/Gallery'
+import Layout from '../components/layout'
+
+const HomeIndex = () => {
+  const siteTitle = 'Konstantin Müllner... hired!'
+  const siteDescription = 'Site description'
+
+  return (
+    <Layout>
+      <Helmet>
+        <title>{siteTitle}</title>
+        <meta name="description" content={siteDescription} />
+      </Helmet>
+
+      <div id="main">
+        <section id="one">
+          <header className="major">
+            <h2>
+              "To be or not to be...
+              <br />
+              not to be" - Arnold Schwarzenegger
+            </h2>
+          </header>
+          <p>
+            Hey, my name is Konstantin, a Full Stack Developer to be. Looking
+            forward to present you all my projects I have worked on and will
+            work on in the future.
+          </p>
+          <ul className="actions">
+            <li>
+              <a href="#" className="button">
+                Learn More
+              </a>
+            </li>
+          </ul>
+        </section>
+
+        <section id="two">
+          <h2>Recent Work</h2>
+
+          <Gallery />
+
+          <ul className="actions">
+            <li>
+              <a href="#" className="button">
+                Full Portfolio
+              </a>
+            </li>
+          </ul>
+        </section>
+
+        <section id="three">
+          <h2>Get In Touch</h2>
+          <p>Would love to hear from you! Just slide into Dm's!</p>
+          <div className="row">
+            <div className="8u 12u$(small)">
+              <form method="post" action="#">
+                <div className="row uniform 50%">
+                  <div className="6u 12u$(xsmall)">
+                    <input
+                      type="text"
+                      name="name"
+                      id="name"
+                      placeholder="Name"
+                    />
+                  </div>
+                  <div className="6u 12u$(xsmall)">
+                    <input
+                      type="email"
+                      name="email"
+                      id="email"
+                      placeholder="Email"
+                    />
+                  </div>
+                  <div className="12u">
+                    <textarea
+                      name="message"
+                      id="message"
+                      placeholder="Message"
+                      rows="4"
+                    ></textarea>
+                  </div>
+                </div>
+                <ul className="actions" style={{ marginTop: 30 }}>
+                  <li>
+                    <input type="submit" value="Send Message" />
+                  </li>
+                </ul>
+              </form>
+            </div>
+            <div className="4u 12u$(small)">
+              <ul className="labeled-icons">
+                <li>
+                  <h3 className="icon fa-home">
+                    <span className="label">Address</span>
+                  </h3>
+                  Vienna - Austria
+                </li>
+                <li>
+                  <h3 className="icon fa-mobile">
+                    <span className="label">Phone</span>
+                  </h3>
+                  +43 6767 / 721 2002
+                </li>
+                <li>
+                  <h3 className="icon fa-envelope-o">
+                    <span className="label">Email</span>
+                  </h3>
+                  <a href="mailto:konstantinmellner@gmail.com">
+                    konstantinmuellner@gmail.com
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </section>
+      </div>
+    </Layout>
+  )
+}
+
+export default HomeIndex
